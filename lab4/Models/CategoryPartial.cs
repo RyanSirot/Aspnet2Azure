79a91597d4da85643faa62c38fe70407b4e2d3cf
RyanSirot <torisnayr@gmail.com>
2017-02-06 17:28:02 -0800

    add

---
+++ lab4/Models/CategoryPartial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab4.Models
+{
+    [MetadataType(typeof(CategoryMetaData))]
+    public partial class Category
+    {
+    }
+
+    public class CategoryMetaData
+    {
+
+  
+        [Display(Name = "Category")]
+        public Object CategoryName { get; set; }
+
+
+    }
+}
---
+++ lab4/Models/SupplierPartial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab4.Models
+{
+    [MetadataType(typeof(SupplierMetaData))]
+    public partial class Supplier
+    {
+    }
+
+    public class SupplierMetaData
+    {
+
+        [Display(Name = "Supplier")]
+        public Object CompanyName { get; set; }
+
+
+    }
+}
